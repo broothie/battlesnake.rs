06e1bf7f5326e547b512b4027718b40fee4c5349
Andrew Booth <adbooth8@gmail.com>
2022-05-20 15:52:59 +0000

    set FOOD_COEFFICIENT to 1.6

--- src/game/state.rs
+++ src/game/state.rs
@@ -8,6 +8,8 @@ use rand::{seq::SliceRandom, thread_rng};
 use serde::Deserialize;
 use std::collections::HashSet;
 
+const FOOD_COEFFICIENT: f32 = 1.5;
+
 #[derive(Deserialize, Debug)]
 pub struct State {
     pub turn: u16,
@@ -38,7 +40,7 @@ impl State {
         println!("valid moves: {:?}", moves);
         if let Some(closest_food) = self.board.closest_food(&self.you.head) {
             let distance = closest_food.distance(&self.you.head);
-            if distance as f64 > self.you.health as f64 * 1.2 {
+            if distance as f32 > self.you.health as f32 * FOOD_COEFFICIENT {
                 let towards = self.you.head.towards(closest_food);
 
                 let move_set: HashSet<Move> = HashSet::from_iter(moves.iter().cloned());
